efc6a5991884dc95dd3e4cf83ec17b4248595697
josam14 <aminselem@icould.com>
2026-06-17 19:48:19 -0600

    modelo customers_totals

---
+++ learn_dbt/models/example/customers_totals.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+select 
+    c.C_CUSTKEY,
+    c.c_NAME,
+    c.C_PHONE,
+    o.O_ORDERSTATUS,
+    sum(o.O_TOTALPRICE) as total_price
+from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.CUSTOMER c
+left join SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.orders o
+on c.C_CUSTKEY=o.o_custkey
+group by 
+    c.C_CUSTKEY,
+    c.c_NAME,
+    c.C_PHONE,
+    o.O_ORDERSTATUS
+order by c.C_CUSTKEY asc
